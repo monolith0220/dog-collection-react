ce035445f8956bc2ed64380e481f31afddaf4258
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-04-18 13:19:16 +0900

    firebase連携

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export const Footer: React.FC = () => {
 	return (
 		<footer className="bg-slate-950 py-3">
 			<div>
-				<p className="text-slate-100">Dog images are retrieved from Dog API</p>
-				<p>
+				<p className="text-slate-100 text-center">Dog images are retrieved from Dog API</p>
+				<p className="text-center">
 					<a className="text-cyan-500" href="https://dog.ceo/dog-api/about">
 						Donate to Dog API
 					</a>
